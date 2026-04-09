697b7ce282d2f6b66fc19e5528bd44f012409ac5
Aman18-18 <amanssingh1827@gmail.com>
2026-04-09 18:42:58 +0530

    Env used instead of making token public

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = "Token";
 
+const token = process.env.BOT_TOKEN;
+const chatId = process.env.CHAT_ID;
 const bot = new TelegramBot(token, { polling: true });
 
-bot.sendMessage("chatId", "Hello there ,Hi!!");
+bot.sendMessage(chatId, "Hello there ,Hi!!");
